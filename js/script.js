b1d4e4e8f9373ae7203532ed6772090da7def9a6
Jesse Gay <jessegay@users.noreply.github.com>
2020-07-28 16:43:31 -0700

    enabled color select upon design select change

--- js/script.js
+++ js/script.js
@@ -6,17 +6,18 @@ name.focus();
 const other = document.getElementById("other-title");
 other.style.display = "none";
 
+// disable color select until design selection is made
+const color = document.getElementById("color");
+color.disabled = true;
+
 // Update the “Color” field to read “Please select a T-shirt theme”.
 const design = document.getElementById("design");
 
 
 design.addEventListener('change', (event) => {
     console.log("selection started in design select");
-
+    color.disabled = false;
 })
 
 
 
-// disable color select until design selection is made
-const color = document.getElementById("color");
-color.disabled = true;
