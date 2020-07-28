1142a11a798400276ebec9e28daf448c9f468a14
Jesse Gay <jessegay@users.noreply.github.com>
2020-07-28 16:05:21 -0700

    Added event listener for 'change' in design select
    When a change happens to the "design" select, something happens. Test console.log for now. Need to enable color selection, which I have disabled by setting .disabled to true.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ name.focus();
 //Hide "job_role_other"
 const other = document.getElementById("other-title");
 other.style.display = "none";
+
+// Update the “Color” field to read “Please select a T-shirt theme”.
+const design = document.getElementById("design");
+
+
+design.addEventListener('change', (event) => {
+    console.log("selection started in design select");
+
+})
+
+
+
+// disable color select until design selection is made
+const color = document.getElementById("color");
+color.disabled = true;
