232a1bb2bfbc8484a9f70e832b7b52d470f2b5a3
Jesse Gay <jessegay@users.noreply.github.com>
2020-07-28 17:32:18 -0700

    Added "placeholder" to color select
    Created an unselectable option with "placeholder" text and added to first index of color select.

--- js/script.js
+++ js/script.js
@@ -6,16 +6,23 @@ name.focus();
 const other = document.getElementById("other-title");
 other.style.display = "none";
 
-// disable color select until design selection is made
+// disable color select until Design selection is made
 const color = document.getElementById("color");
 color.disabled = true;
 
 // Update the “Color” field to read “Please select a T-shirt theme”.
+// Create new option
+const colorPlaceholder = document.createElement('option');
+colorPlaceholder.text = 'Please select a T-shirt theme';
+// make it unselectable
+colorPlaceholder.disabled = true;
+// add it to color select at beginning of list
+color.add(colorPlaceholder, color.options[0]);
+color.selectedIndex = '0';
+
+// Enable color selection once Design is selected
 const design = document.getElementById("design");
-
-
 design.addEventListener('change', (event) => {
-    console.log("selection started in design select");
     color.disabled = false;
 })
 
