13d053af74e07493f635ce3de88a78fd7d707018
Jesse Gay <jessegay@users.noreply.github.com>
2020-08-31 17:42:08 -0700

    Added checkbox exclusivity
    Used checkboxes warmup as a template.
    Not sure if this meets requirements for "and visually indicate that the workshop in the competing time slot isn't available."
    The unavailable ones are grayed out, but it's subtle.

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ design.addEventListener('change', (event) => {
     color.selectedIndex = '0';
     //If user selects Puns, ♥ options are hidden
     if (event.target.value == 'js puns') {
-        // loop through color and hide options whose text contains '♥' (for some reason I can't use the decimal equivalent)
+        // loop through color and hide options whose text contains '♥' 
         var i;
          for (i = 0; i < color.length; i++) {
             if (color.options[i].text.includes('♥')) {
@@ -53,20 +53,53 @@ design.addEventListener('change', (event) => {
   
 })
 
-
-
-
-
-
-// Up next
 /*
 For the T-Shirt "Color" menu, after a user selects a theme, only display the color options that match the design selected in the "Design" menu.
 If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
 If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
  
 Instructions say to do this inside the event listener. My attempt starts at line 27 
-
-When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated. Hmmmm, the 
-options are limited correctly, but I need to find a way to reset the color field to colorOptions[0]
 */
 
+// Up next
+/*
+Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- 
+you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+
+When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+
+As a user selects activities, a running total should display below the list of checkboxes. 
+For example, if the user selects "Main Conference", then Total: $200 should appear. 
+If they add 1 workshop, the total should change to Total: $300.
+
+data-day-and-time="Tuesday 9am-12pm"
+data-day-and-time="Tuesday 1pm-4pm"
+
+*/
+//create array to store all checkboxes
+const checkboxes = document.querySelectorAll('.activities input');
+//console.log(checkboxes);
+document.querySelector('.activities').addEventListener('change', (e) => {
+    const clicked = e.target;
+    const clickedType = e.target.getAttribute('data-day-and-time');
+    for (let i = 0; i < checkboxes.length; i ++) {
+        let checkboxType = checkboxes[i].getAttribute('data-day-and-time');
+        if (clickedType === checkboxType && clicked !== checkboxes[i]) {
+            if (clicked.checked) {
+                checkboxes[i].disabled = true;
+            } else {
+                checkboxes[i].disabled = false;
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+})
